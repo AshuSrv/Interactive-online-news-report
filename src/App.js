babb5c02adb90da981ab4d13cf8071917e270178
Ashutosh Shrivastava <ashutoshmishra422@gmail.com>
2025-04-13 00:29:29 +0100

    Added main App.js page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import "./App.css";
 import Introduction from "./components/Introduction";
 import { Container, MainArticle } from "./assets/styles";
 import PolicyArchitecture from "./components/PolicyArchitecture";
-// import ImpactAnalysis from "./components/ImpactAnalysis";
-// import IndustryImpact from "./components/IndustryImpact";
+import ImpactAnalysis from "./components/ImpactAnalysis";
+import IndustryImpact from "./components/IndustryImpact";
 // import FutureScenarios from "./components/FutureScenarios";
+import Sources from "./components/Sources";
 
 const App = () => {
   return (
@@ -18,12 +19,12 @@ const App = () => {
         {/* Section 2: Policy Architecture with Video */}
         <PolicyArchitecture />
         {/* Section 3: Policy Architecture with Video */}
-        {/* <ImpactAnalysis /> */}
-
+        <ImpactAnalysis />
         {/* Section 4: Industry Impacts */}
-        {/* <IndustryImpact /> */}
+        <IndustryImpact />
         {/* Section 5: Future Scenarios */}
         {/* <FutureScenarios /> */}
+        <Sources />
       </MainArticle>
     </Container>
   );
